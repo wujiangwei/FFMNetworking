e85ec710052f56945b68390a8b70486cec8af8e0
wujiangwei <461647731@qq.com>
2014-09-26 15:44:49 +0800

    1.修复bug
    1.上传demo内容解析文件
    2.目前解析文件中不能包含一些url的特殊字符，否则会导致解析失败
    3.自动头文件功能暂未加入

--- IOSModelParse.py
+++ IOSModelParse.py
@@ -2,6 +2,7 @@
 __author__ = 'wujiangwei'
 
 import json
+import re
 
 #config file,your can change anything here
 yourProjectPrefix = 'JF'        #default
@@ -26,24 +27,25 @@ def startParseFiles():
             for line in fileFo.readlines():
                 addPreSuf = ':'
                 lineStr = line.strip('\n')
+                lineStr = line.strip()
                 if(lineStr.count(addPreSuf) >= 1):
                     Location = lineStr.find(addPreSuf)
                     sufStr = lineStr[Location :]
                     dealStr = lineStr[: Location]
+                    #TODO:"如果这边key已经带了""就不要加了
                     dealStr = "\"" + dealStr + "\""
                     lineStr = dealStr + sufStr
-                else:
-                    print 'please enter json format string'
+                    #额外不规则数据的处理
+                    lineStr.replace("'", "\"")
                 easyFileContent = easyFileContent + lineStr
 
+            easyFileContent = re.sub(r"(,?)(\w+?)\s*?:\s*(?='|\d|\[|{)", r"\1'\2':", easyFileContent)
             decodejson = json.loads(easyFileContent)
             fileFo.close()
 
             generationFileByDict(fileName, transferJsonToDic(decodejson), 0)
         except IOError:
             print 'can not find your file named' + fileName + IOError.message
-        except ValueError:
-            print ValueError.message
 
 def FirstStrBigger(str):
     firStr = str[:1]
@@ -70,7 +72,7 @@ def generationFileByDict(fileName, aDict, needDicKey):
         #@end
         mFileName = className + '.m'
         OjectCMFile = open(mFileName, 'w')
-        OjectCMFile.write('////Auto ' + className + '.m File by Python Script,Kevin\n\n\n')
+        OjectCMFile.write('//\n//Auto ' + className + '.m File \n//From Python Script Kevin\n//\n//https://github.com/wujiangwei/ModelNetworkClient\n\n')
         OjectCMFile.write('#import \"' + className + '.h\"\n\n')
         OjectCMFile.write('@implementation ' + className)
         OjectCMFile.write('\n')
@@ -97,23 +99,26 @@ def generationFileByDict(fileName, aDict, needDicKey):
             ListKey = 'NSArray'
 
             #write .h File header
-            OjectCFile.write('////Auto ' + className + '.h File by Python Script,Kevin\n\n\n\n')
-            OjectCFile.write('#import "JSONModel.h"\n\n')
+            OjectCFile.write('//\n// Auto Create JsonModel File\n// ' + className + '.h' +  '\n//\n// Github: https://github.com/wujiangwei/ModelNetworkClient\n\n\n')
+            OjectCFile.write('#import "JSONModel.h"\n')
             #need protocol
             if(needDicKey == 2):
                 #@protocol albumListItemModel
                 #@end
-                OjectCFile.write('@protocol ' + className)
-                OjectCFile.write('\n')
-                OjectCFile.write('@end\n\n')
+                OjectCFile.write('\n@protocol ' + className)
+                OjectCFile.write('\n\n')
+                OjectCFile.write('@end')
 
-            OjectCFile.write('@interface ' + className + ' : ' + yourModelBaseClassName)
-            OjectCFile.write('\n')
-            OjectCFile.write('\n')
+            OjectCFile.write('\n\n@interface ' + className + ' : ' + yourModelBaseClassName)
+            OjectCFile.write('\n\n')
+
         else:
             #Android
             protocol = 'implements'
             #TODO For Android:
+
+    if(isinstance(aDict, list) or isinstance(aDict, tuple)):
+        return ""
 
     for key in aDict:
         print "aDict[%s] =" %key, aDict[key]
@@ -125,46 +130,33 @@ def generationFileByDict(fileName, aDict, needDicKey):
         #@property (nonatomic, strong)albumListItemModel<Optional> *testModel;
 
         if(isinstance(value, list) or isinstance(value, tuple)):
-
             protocolKeyJsonM = generationFileByDict(key, parseDicFromList(value), 2)
-            LineContent = '@property (nonatomic, strong) ' + ListKey + '<Optional, ' + protocolKeyJsonM + '> *' + key + ';'
-            OjectCFile.write('\n')
-            OjectCFile.write('\n')
+            LineContent = ''
+            if(len(protocolKeyJsonM) > 0):
+                LineContent = '\n@property (nonatomic, strong) ' + ListKey + '<Optional, ' + protocolKeyJsonM + '> *' + key + ';\n'
+            else:
+                LineContent = '@property (nonatomic, strong) ' + ListKey + '<Optional> *' + key + ';\n'
             OjectCFile.write(LineContent)
-            OjectCFile.write('\n')
-            OjectCFile.write('\n')
-            #深度遍历,目前只支持 Dic，如果是其他单对象，请联系461647731修改
-            #以后会加入默认key
-
+            #若是数组会加入默认key
 
         elif(isinstance(value, str) or isinstance(value, unicode)):
-            LineContent = '@property (nonatomic, strong) ' + Strkey + '<Optional> *' + key + ';'
-            OjectCFile.write('\n')
+            LineContent = '@property (nonatomic, strong) ' + Strkey + '<Optional> *' + key + ';\n'
             OjectCFile.write(LineContent)
-            OjectCFile.write('\n')
 
         elif(isinstance(value, dict)):
             objectKey = generationFileByDict(key, value, 1)
-            OjectCFile.write('\n')
-            OjectCFile.write('\n')
-            LineContent = '@property (nonatomic, strong) ' + objectKey + '<Optional> *' + key + ';'
+            LineContent = '\n@property (nonatomic, strong) ' + objectKey + '<Optional> *' + key + ';\n'
             OjectCFile.write(LineContent)
-            OjectCFile.write('\n')
-            OjectCFile.write('\n')
 
-        elif(isinstance(value, int) or isinstance(value, long) or isinstance(value, float) or \
-                   isinstance(value, False) or isinstance(value, True)):
-            LineContent = '\n@property (nonatomic, strong) ' + IntKey + '<Optional> *' + key + ';' + '\n'
+        elif(isinstance(value, int) or isinstance(value, long) or isinstance(value, float)):
+            LineContent = '@property (nonatomic, strong) ' + IntKey + '<Optional> *' + key + ';' + '\n'
             OjectCFile.write(LineContent)
 
         else:
             #Null or other Object
-            LineContent = '@property (nonatomic, strong) ' + Strkey + '<Optional> *' + key + ';'
-            OjectCFile.write('\n')
+            LineContent = '@property (nonatomic, strong) ' + Strkey + '<Optional> *' + key + ';\n'
             OjectCFile.write(LineContent)
-            OjectCFile.write('\n')
 
-    OjectCFile.write('\n')
     OjectCFile.write('\n')
     OjectCFile.write('@end')
 
@@ -174,14 +166,11 @@ def generationFileByDict(fileName, aDict, needDicKey):
 
 def parseDicFromList(aList):
     #the object in List,here must be Object,and not a string or int/null
-    if(not (isinstance(aList, list) or isinstance(aList, tuple))):
-        return {'new_error' : 'not list'}
-
     if(len(aList) <= 0):
-        return {'new_error' : 'list is empty'}
+        return {'new_error_list_empty_write_model_yourself' : 'list is empty'}
     if(isinstance(aList[0], dict)):
         return aList[0]
-    return {defaultSubObjectKey : aList[0]}
+    return aList
 
 def transferJsonToDic(decodejson):
     #Array List
